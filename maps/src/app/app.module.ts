dc45decd28656abacd597c991e87c6198ba5ceb4
jailsonpj <jailsoncolares@gmail.com>
2017-12-29 12:35:49 -0400

    config feitas

--- maps/src/app/app.module.ts
+++ maps/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {Geolocation} from '@ionic-native/geolocation';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import {Geolocation} from '@ionic-native/geolocation';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- maps/src/pages/home/home.ts
+++ maps/src/pages/home/home.ts
@@ -1,21 +1,73 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Geolocation} from '@ionic-native/geolocation';
+import { AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 declare var google;
+let lista = [];
+let listas = {lati: {},long: {},local1: {},fazendo1: {},hora: {h: {},m: {},s: {}} };
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
-  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public geolocation: Geolocation,private alertCtrl: AlertController, private storage: Storage) {
 
   }
+
+
+	presentPrompt() {
+  		let alert = this.alertCtrl.create({
+    	title: 'Onde vc está?',
+    	inputs: [
+      	{
+        	name: 'local',
+        	placeholder: 'Local'
+      	},
+      	{
+        	name: 'fazendo',
+        	placeholder: 'O que você está fazendo?',
+        	
+      	}
+    ],
+    buttons: [
+      {
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Registrar',
+        handler: data => {
+            
+            listas.local1 = data.local;
+            listas.fazendo1 = data.fazendo;
+            //listas.hora = Date.prototype.setUTCDate();
+            
+            this.storage.set(data.local,lista.push(listas));
+            
+            console.log(listas);
+            console.log(lista);
+            // logged in! 
+
+            console.log('Registrar clicked');
+
+        }
+      }
+    ]
+  });
+  alert.present();
+}
+
 
   ionViewDidLoad(){
   	this.loadMap();
@@ -26,6 +78,13 @@ export class HomePage {
 
 
   			let latLng = new google.maps.LatLng(position.coords.latitude, 	position.coords.longitude);
+  		  
+        listas.lati = position.coords.latitude;
+        listas.long = position.coords.longitude;
+        let data = new Date;
+        listas.hora.h = data.getHours();
+        listas.hora.m = data.getMinutes();
+        listas.hora.s = data.getSeconds();
 
   			let mapOptions = {
   				center: latLng,
@@ -40,15 +99,23 @@ export class HomePage {
   	
 
   	addMarker(){
-  		let marker = new google.maps.Marker({
-  			map:this.map,
-  			animation: google.maps.Animation.DROP,
-  			position: this.map.getCenter()
+  		this.geolocation.getCurrentPosition().then((position) => {
+
+
+  			let latLng = new google.maps.LatLng(position.coords.latitude, 	position.coords.longitude);
+  			let marker = new google.maps.Marker({
+  				map:this.map,
+  				animation: google.maps.Animation.DROP,
+  				position: latLng
+
+  			});
+  			let content = "<h4>Local Atual!</h4>"
+
+  			this.addInfoWindow(marker,content);
+  			
+  		},(err) => {
+  			console.log(err);
   		});
-
-  		let content = "<h4>Local Atual!</h4>";
-
-  		this.addInfoWindow(marker,content);
   	}
 
   	addInfoWindow(marker,content){
@@ -57,6 +124,7 @@ export class HomePage {
   		});
   		google.maps.event.addListener(marker,'click',() => {
   			infoWindow.open(this.map,marker);
+  			this.presentPrompt();
   		});
   	}
 }
